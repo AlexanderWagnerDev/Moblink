b937872aa5dd9de7c1a6a5e081bd459ce84b8a9e
Erik Moqvist <erik.moqvist@gmail.com>
2025-09-11 06:42:08 +0200

    Version 0.42.0.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.eerimoq.moblink"
         minSdk = 26
         targetSdk = 35
-        versionCode = 41
-        versionName = "0.41.0"
+        versionCode = 42
+        versionName = "0.42.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
